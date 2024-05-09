91fae0bf2bb1536c003b1ad175b65b17c33cc285
chg <chg@dsfs004575.abatgroup.de>
2024-05-09 17:55:06 +0200

    initial commit

--- JDLogger/src/de/g3ll3rt/jdlogger/Settings.java
+++ JDLogger/src/de/g3ll3rt/jdlogger/Settings.java
@@ -1,6 +1,6 @@
 package de.g3ll3rt.jdlogger;
 
-public class Settings {
+public class Settings { 
 	public static final String deyeURL= "http://192.168.2.66"; 
 	public static final String deyeStatusPath = "/status.html";
 	public static final String deyeUserName = "admin";
